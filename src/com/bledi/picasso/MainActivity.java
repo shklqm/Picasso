ef4b5c44d239659380374e3f52018cc25cfff489
Shkelqim Memolla <shkelqim.memolla@gmail.com>
2014-07-02 18:03:37 +0300

    Working

--- src/com/bledi/picasso/MainActivity.java
+++ src/com/bledi/picasso/MainActivity.java
@@ -2,20 +2,23 @@ package com.bledi.picasso;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
+import android.widget.ImageView;
 import android.widget.ListView;
 
 public class MainActivity extends Activity {
 
+	ImageView imageView;
+	//ListView listView;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
-        
+        ListView listView = ( ListView ) findViewById(R.id.listview);
+        	
         ExampleViewModel firstRow = new ExampleViewModel("First Row", "http://upload.wikimedia.org/wikipedia/commons/6/6f/Freiburger_Alpen.JPG");    
         ExampleViewModel secondRow = new ExampleViewModel("Second Row", "http://blog.caranddriver.com/wp-content/uploads/2013/05/lamborghini_egoista_three_quarter_front_view.jpg");    
         ExampleViewModel thirdRow = new ExampleViewModel("Third Row", "http://4.bp.blogspot.com/-vXnf7GjcXmg/UfJZE9rWc2I/AAAAAAAAGRc/x2CIlHM9IAA/s1600/aphoto49721.jpg");
@@ -26,16 +29,7 @@ public class MainActivity extends Activity {
         viewModels.add(thirdRow);
         
         ExampleAdapter adapter = new ExampleAdapter(this, viewModels);
-        ListView.setAdapter(adapter);
+        listView.setAdapter(adapter);
      
     }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
 }
